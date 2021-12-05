0cd287f4e67f5e9da67ef89a74b70afbb39b6893
tanmaij <killdogvinam@gmail.com>
2021-12-05 20:46:59 +0700

    add route get notify with accessToken

--- api/routes/notify.js
+++ api/routes/notify.js
@@ -29,7 +29,6 @@ const notifyRouter = (io) => {
     return res.status(200).json({ message: "Thành công", success: true });
   });
   router.patch("/read-all/", verifyToken, async (req, res) => {
-    const id = req.params.id;
     const getUser = await user.findById(req.user.id);
     await user.findOneAndUpdate(
       { _id: req.user.id, deleted: false },
@@ -43,6 +42,59 @@ const notifyRouter = (io) => {
     );
     return res.status(200).json({ message: "Thành công", success: true });
   });
+  router.get("/", verifyToken, async (req, res) => {
+    try {
+      const getUser = await user.findById(req.user.id);
+      const { _page, _limit, _read } = req.query;
+      if (!getUser)
+        return res
+          .status(400)
+          .json({ success: false, message: "Không tìm thấy người dùng" });
+      let responseNotify = [...getUser.notify].sort((n1, n2) => {
+        return new Date(n2.createdAt) - new Date(n1.createdAt);
+      });
+      let read = responseNotify.filter((item) => item.read == true).length,
+        unread = responseNotify.filter((item) => item.read == false).length;
+      if (Array.isArray(_read))
+        if (
+          (_read[0].toLowerCase() === "true" ||
+            _read[0].toLowerCase() === "false") &&
+          (_read[1].toLowerCase() === "true" ||
+            _read[1].toLowerCase() === "false")
+        ) {
+          if (_read[0].toLowerCase() === "false")
+            responseNotify = responseNotify.filter((item) => {
+              return item.read == false;
+            });
+          if (_read[1].toLowerCase() === "false")
+            responseNotify = responseNotify.filter((item) => {
+              return item.read == true;
+            });
+        }
+      let page = 1,
+        limit = responseNotify.length,
+        totalRows = responseNotify.length;
+
+      if (!isNaN(parseInt(_page)) && !isNaN(parseInt(_limit))) {
+        page = parseInt(_page);
+        limit = parseInt(_limit);
+      }
+      responseNotify = responseNotify.slice((page - 1) * limit, limit * page);
+      res.status(200).json({
+        success: true,
+        data: { notify: responseNotify, read, unread },
+        pagination: {
+          _page: page,
+          _limit: limit,
+          _totalRows: totalRows,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "Lỗi không xác định", success: false });
+    }
+  });
+
   return router;
 };
 
